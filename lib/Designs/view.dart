c35e3dd27c54d03710ff4a953d5a07d65ce0f6f8
Rishabh Sethia <rishabh@Rishabhs-MacBook-Air.local>
2021-11-21 15:06:41 +0530

    Finished

--- lib/Designs/view.dart
+++ lib/Designs/view.dart
@@ -34,171 +34,183 @@ class View extends StatelessWidget {
           onPressed: () {},
         ),
       ),
-      body: ListView(
-        shrinkWrap: true,
-        children: [
-          SizedBox(
-            height: windowHeight * 0.42,
-            child: Column(
-              children: [
-                Container(
-                  height: windowHeight * 0.3,
-                  width: windowWidth,
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade200,
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        viewController.airData.value.data!.imageUrl.toString(),
-                      ),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 12.0, vertical: 8.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.asset('Assets/Images/ac-hx-02.png'),
-                      SizedBox(width: windowWidth * 0.05),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              viewController.airData.value.data!.categoryName
-                                  .toString(),
-                              style: const TextStyle(
-                                fontSize: 23,
-                                fontWeight: FontWeight.bold,
+      body: Obx(
+        () => viewController.airData.value.data == null
+            ? const Offstage()
+            : ListView(
+                shrinkWrap: true,
+                children: [
+                  SizedBox(
+                    height: windowHeight * 0.42,
+                    child: Column(
+                      children: [
+                        Container(
+                          height: windowHeight * 0.3,
+                          width: windowWidth,
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade200,
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                viewController.airData.value.data!.imageUrl
+                                    .toString(),
                               ),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(
-                              viewController.airData.value.data!.description
-                                  .toString(),
-                              style: const TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Divider(
-            color: Colors.grey.shade300,
-            thickness: 6,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
-            child: Center(
-              child: Text(
-                'Choose Services',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.grey.shade600,
-                ),
-              ),
-            ),
-          ),
-          Divider(
-            color: Colors.grey.shade300,
-            thickness: 2,
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: viewController.airData.value.data!.services!.length,
-            itemBuilder: (BuildContext context, int index) {
-              if (index ==
-                  viewController.airData.value.data!.services!.length) {
-                return SizedBox(height: windowHeight * 0.1);
-              } else {
-                return Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 12.0, vertical: 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: windowHeight * 0.08,
-                            width: windowWidth * 0.18,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: Colors.grey.shade200,
-                              image: DecorationImage(
-                                image: NetworkImage(
-                                  viewController.airData.value.data!
-                                      .services![index].imageUrl
-                                      .toString(),
-                                ),
-                                fit: BoxFit.cover,
-                              ),
+                              fit: BoxFit.cover,
                             ),
                           ),
-                          Column(
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 12.0, vertical: 8.0),
+                          child: Row(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(
-                                viewController.airData.value.data!
-                                    .services![index].serviceName
-                                    .toString(),
-                                style: const TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.w500),
-                              ),
-                              Text(
-                                "Avg Cost: " +
-                                    viewController.airData.value.data!
-                                        .services![index].rate
-                                        .toString() +
-                                    " Sar",
-                                style: const TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w400,
-                                    color: Colors.red),
-                              ),
-                              const SizedBox(height: 5),
-                              Text(
-                                viewController.airData.value.data!
-                                    .services![index].description
-                                    .toString(),
-                                style: TextStyle(
-                                    color: Colors.grey.shade500, fontSize: 15),
+                              Image.asset('Assets/Images/ac-hx-02.png'),
+                              SizedBox(width: windowWidth * 0.05),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      viewController
+                                          .airData.value.data!.categoryName
+                                          .toString(),
+                                      style: const TextStyle(
+                                        fontSize: 23,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 5),
+                                    Text(
+                                      viewController
+                                          .airData.value.data!.description
+                                          .toString(),
+                                      style: const TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w500,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ],
                           ),
-                          AddButton(
-                            windowHeight: windowHeight,
-                            windowWidth: windowWidth,
-                            onPressed: () {},
-                          ),
-                        ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.grey.shade300,
+                    thickness: 6,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: Center(
+                      child: Text(
+                        'Choose Services',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.grey.shade600,
+                        ),
                       ),
                     ),
-                    Divider(
-                      color: Colors.grey.shade300,
-                      thickness: 2,
-                    ),
-                  ],
-                );
-              }
-            },
-          ),
-          // SizedBox(
-          //   height: windowHeight,
-          // ),
-        ],
+                  ),
+                  Divider(
+                    color: Colors.grey.shade300,
+                    thickness: 2,
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount:
+                        viewController.airData.value.data!.services!.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      if (index ==
+                          viewController.airData.value.data!.services!.length) {
+                        return SizedBox(height: windowHeight * 0.1);
+                      } else {
+                        return Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 12.0, vertical: 8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    height: windowHeight * 0.08,
+                                    width: windowWidth * 0.18,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.grey.shade200,
+                                      image: DecorationImage(
+                                        image: NetworkImage(
+                                          viewController.airData.value.data!
+                                              .services![index].imageUrl
+                                              .toString(),
+                                        ),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        viewController.airData.value.data!
+                                            .services![index].serviceName
+                                            .toString(),
+                                        style: const TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                      Text(
+                                        "Avg Cost: " +
+                                            viewController.airData.value.data!
+                                                .services![index].rate
+                                                .toString() +
+                                            " Sar",
+                                        style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w400,
+                                            color: Colors.red),
+                                      ),
+                                      const SizedBox(height: 5),
+                                      Text(
+                                        viewController.airData.value.data!
+                                            .services![index].description
+                                            .toString(),
+                                        style: TextStyle(
+                                            color: Colors.grey.shade500,
+                                            fontSize: 15),
+                                      ),
+                                    ],
+                                  ),
+                                  AddButton(
+                                    windowHeight: windowHeight,
+                                    windowWidth: windowWidth,
+                                    onPressed: () {},
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Divider(
+                              color: Colors.grey.shade300,
+                              thickness: 2,
+                            ),
+                          ],
+                        );
+                      }
+                    },
+                  ),
+                  // SizedBox(
+                  //   height: windowHeight,
+                  // ),
+                ],
+              ),
       ),
     );
   }
