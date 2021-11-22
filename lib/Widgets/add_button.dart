df54cbed4c869d4dc15cef771dd9c330f7ad8fa4
Rishabh Sethia <rishabh@Rishabhs-MacBook-Air.local>
2021-11-22 11:02:10 +0530

    Button Press for add

--- lib/Widgets/add_button.dart
+++ lib/Widgets/add_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class AddButton extends StatelessWidget {
-  const AddButton({
+class AddButton extends StatefulWidget {
+  AddButton({
     Key? key,
     required this.windowHeight,
     required this.windowWidth,
@@ -11,23 +11,45 @@ class AddButton extends StatelessWidget {
   final double windowHeight;
   final double windowWidth;
   final Function onPressed;
+  Color color = Colors.grey.shade300;
+  int pressed = 1;
+  String text = 'Add';
 
+  @override
+  State<AddButton> createState() => _AddButtonState();
+}
+
+class _AddButtonState extends State<AddButton> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onPressed as void Function(),
+      onTap: () {
+        if (widget.pressed == 1) {
+          setState(() {
+            widget.color = Colors.yellow;
+            widget.text = 'Added';
+            widget.pressed = 2;
+          });
+        } else if (widget.pressed == 2) {
+          setState(() {
+            widget.color = Colors.grey.shade300;
+            widget.text = 'Add';
+            widget.pressed = 1;
+          });
+        }
+      },
       child: Container(
-        height: windowHeight * 0.04,
-        width: windowWidth * 0.2,
+        height: widget.windowHeight * 0.04,
+        width: widget.windowWidth * 0.2,
         decoration: BoxDecoration(
-          color: Colors.grey.shade300,
+          color: widget.color,
           borderRadius: BorderRadius.circular(20),
         ),
         child: Center(
           child: Text(
-            'Add',
+            widget.text,
             style: TextStyle(
-              fontSize: windowHeight * 0.02,
+              fontSize: widget.windowHeight * 0.02,
               fontWeight: FontWeight.w400,
               color: Colors.black,
             ),
